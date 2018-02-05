adb4359924ef96af629ab5348711fc2da0fadd06
Simon Brookes <simon.brookes@btinternet.com>
2018-02-05 01:05:06 +0000

    Sorted usings

--- SecurityDemo1/Startup.cs
+++ SecurityDemo1/Startup.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using SecurityDemo1.Data;
 using SecurityDemo1.Models;
 using SecurityDemo1.Services;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Authentication;
 
 namespace SecurityDemo1
 {
